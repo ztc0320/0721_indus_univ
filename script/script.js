1b7bea764b223b2509abb513998169df3acf79b8
ztc0320 <rjdrjsdn@naver.com>
2021-07-22 16:15:28 +0900

    slide script

--- script/script.js
+++ script/script.js
@@ -7,4 +7,18 @@ $(function(){
         $(".subMenu").stop().slideUp(300);
         $("div#subMenuBG").stop().slideUp(300);
     });
+
+    // 슬라이드쇼 시작
+    setInterval(fnSlide, 3000);
+    function fnSlide() {
+        $("#shuttleFrame").animate(
+            {"margin-left": "-1200px"},
+            2400,
+            function(){
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+                $("#shuttleFrame").css({"margin-left": "0"});
+            } 
+        );
+    }
+    // 슬라이드쇼 끝
 });
