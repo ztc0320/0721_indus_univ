1296f823e76afbed97f4a4e90e2660438f80d8ca
ztc0320 <rjdrjsdn@naver.com>
2021-07-22 14:45:30 +0900

    0722_GNB작업완료

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $(".mainLi a").mouseover(function(){
+        $(".subMenu").stop().slideDown(800);
+        $("div#subMenuBG").stop().slideDown(800);
+    });
+    $(".mainLi a").mouseout(function(){
+        $(".subMenu").stop().slideUp(300);
+        $("div#subMenuBG").stop().slideUp(300);
+    });
 });
